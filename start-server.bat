709ed300dd3997d214bbecca5830635421b4d905
Ahmed Ali <ahmedaliqurexhi7867@gmail.com>
2025-07-03 00:03:30 +0500

    Update start-server.bat

--- start-server.bat
+++ start-server.bat
@@ -1,18 +1,1 @@
-@echo off
-title Video Downloader Server
 
-:: Change to your Node.js server folder
-cd /d C:\Your\Path\To\Project
-
-:: Update yt-dlp (Python version)
-echo Updating yt-dlp...
-python -m pip install -U yt-dlp
-
-:: Optional: Add yt-dlp to PATH (if needed)
-:: set PATH=%PATH%;C:\Path\To\yt-dlp-folder
-
-:: Start Node server
-echo Starting Node.js server...
-node app.js
-
-pause
